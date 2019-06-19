9725eaeb1e70d14e1898a69c9de448065f88e3e7
Serena Hong <serenahong.usa@gmail.com>
2019-06-19 10:29:46 -0600

    blogs with class component

---
+++ client/src/components/Blogs.js
@@ -0,0 +1,40 @@
+import React from "react";
+import axios from "axios";
+import { List, Header, Segment, } from "semantic-ui-react";
+
+class Blogs extends React.Component {
+  state = { blogs: [], };
+
+  componentDidMount() {
+    axios.get("/api/blogs")
+      .then( res => {
+        this.setState({ blogs: res.data, });
+      })
+  }
+
+  renderBlogs = () => {
+    const { blogs, } = this.state;
+    return blogs.map( blog => (
+      <Segment key={blog.id}>
+        <List.Header as="h3">{blog.title}</List.Header>
+        <List.Description>
+          { blog.body }
+        </List.Description>
+      </Segment>
+    ))
+  }
+
+  render() {
+    return (
+      <>
+        <Header as="h1">My Blogs</Header>
+        <br />
+        <List>
+          { this.renderBlogs() }
+        </List>
+      </>
+    )
+  }
+}
+
+export default Blogs;
